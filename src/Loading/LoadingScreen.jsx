70118192fe3bc6e80f334ca28c5360ac6aca57ef
shriram <choudharyshriram53@gmail.com>
2023-04-09 11:10:10 +0530

    feat:made question app

---
+++ src/Loading/LoadingScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+  return (
+    <main>
+        <div className='loading'>
+            
+        </div>
+    </main>
+  )
+}
+
+export default LoadingScreen
